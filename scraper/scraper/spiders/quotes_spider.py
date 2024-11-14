f0082365d9a32dfedbde8d17693de38793df1928
Joshua Ferreira <Kointaker9@gmail.com>
2024-11-14 11:45:38 -0500

    Changed Quotes_spider to include extraction logic

--- scraper/scraper/spiders/quotes_spider.py
+++ scraper/scraper/spiders/quotes_spider.py
@@ -6,13 +6,15 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
-    urls = [
+    start_urls = [
         "https://quotes.toscrape.com/page/1/",
         "https://quotes.toscrape.com/page/2/",
     ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
+        for quote in response.css("div.quote"):
+            yield {
+                "text": quote.css("span.text::text").get(),
+                "author": quote.css("small.author::text").get(),
+                "tags": quote.css("div.tags a.tag::text").getall(),
+            }
